0b43ccbaca7f95c98efd2d0f9a2d9dc703755b9d
KhaledDK <dkhaled794@gmail.com>
2025-04-16 13:25:43 +0200

    fourth.3 commit

---
+++ puppyage.py
@@ -0,0 +1,9 @@
+def puppy_age(human_years):
+
+    return human_years * 7
+
+human_years = int(input("enter your dogs age in human years: "))
+
+dog_years = puppy_age(human_years)
+
+print("your dogs age is", dog_years,"years old in puppy years!") 
